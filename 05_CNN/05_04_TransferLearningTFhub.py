cac2f47dc5c21009c10682e24348be8a043385f1
Mikyeong <leemik1117@gmail.com>
2021-07-26 18:45:14 +0900

    chapter 5 특성추출

--- 05_CNN/05_04_TransferLearningTFhub.py
+++ 05_CNN/05_04_TransferLearningTFhub.py
@@ -13,13 +13,100 @@ chapter5. 합성곱 신경망 1
 1-1) 텐서플로 허브
 - 구글에서 공개한 API
 - 모델에서 재사용 가능한 부분을 게시, 검색, 사용하기 위한 api 제공
-
+'''
 
 import tensorflow as tf
 import tensorflow_hub as hub
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import numpy as np
 
 model = tf.keras.Sequential([
-    hub.KerasLayer('https://tf')
+    hub.KerasLayer('https://tfhub.dev/google/imagenet/resnet_v2_152/feature_vector/4',
+                   input_shape=(224,224,3),
+                   trainable=False),
+    tf.keras.layers.Dense(2, actication='softmax')
 ])
 
-'''
+
+# 모델 훈련
+BATCH_SIZE = 32
+image_height = 100
+image_width = 100
+train_dir = '../data/chap5/data/catanddog/train'
+valid_dir = '../data/chap5/data/catanddog/validation'
+
+
+# 데이터 확장
+train = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1
+)
+
+train_generator = train.flow_from_directory(train_dir,
+                                            target_size=(image_height, image_width),
+                                            color_mode='rgb',
+                                            batch_size=BATCH_SIZE,
+                                            seed=1,
+                                            shuffle=True,
+                                            class_mode='categorical')
+
+valid = ImageDataGenerator(rescale=1.0/255.0)
+valid_generator = valid.flow_from_directory(valid_dir,
+                                            target_size=(image_height, image_width),
+                                            color_mode='rgb',
+                                            batch_size=BATCH_SIZE,
+                                            seed=7,
+                                            shuffle=True,
+                                            class_mode='categorical')
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(train_generator,
+                    epochs=10,
+                    validation_data=valid_generator,
+                    verbose=2)
+
+
+# 모델의 정확도를 시각적으로 표현
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(accuracy))
+
+plt.plot(epochs, accuracy, label='훈련 데이터셋')
+plt.plot(epochs, val_accuracy, label='검증 데이터셋')
+plt.legend()
+plt.title('정확도')
+plt.figure()
+
+plt.plot(epochs, loss, label='훈련 데이터셋')
+plt.plot(epochs, val_loss, label='검증 데이터셋')
+plt.legend()
+plt.title('오차')
+
+
+# 이미지에 대한 예측 확인
+class_names=['cat','dog']
+validation, label_batch = next(iter(valid_generator))
+prediction_values = model.prdict_classes(validation)
+
+fig = plt.figure(figsize=(12,8))
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+for i in range(8):
+    ax = fig.add_subplot(2, 4, i+1, xticks=[], yticks=[])
+    ax.imshow(validation[i,:], cmap=plt.cm.gray_r, interpolation='nearest')
+    if prediction_values[i] == np.argmax(label_batch[i]):
+        ax.text(3, 17, class_names[prediction_values[i]], color='yellow', fontsize=14)
+    else:
+        ax.text(3, 17, class_names[prediction_values[i]], color='red', fontsize=14)
